28057fc3921b1e943a4aa9c3e6455644dc5f0ccc
Chris Ha <chunghha@users.noreply.github.com>
2023-06-06 01:39:00 -0500

    build(lib): TS v5.1.x

--- prisma/pothos-types.ts
+++ prisma/pothos-types.ts
@@ -1,16 +1,18 @@
 /* eslint-disable */
 import type { Prisma, SeoulRent } from "@prisma/client";
 export default interface PrismaTypes {
-  SeoulRent: {
-    Name: "SeoulRent";
-    Shape: SeoulRent;
-    Include: never;
-    Select: Prisma.SeoulRentSelect;
-    OrderBy: Prisma.SeoulRentOrderByWithRelationInput;
-    WhereUnique: Prisma.SeoulRentWhereUniqueInput;
-    Where: Prisma.SeoulRentWhereInput;
-    RelationName: never;
-    ListRelations: never;
-    Relations: {};
-  };
+    SeoulRent: {
+        Name: "SeoulRent";
+        Shape: SeoulRent;
+        Include: never;
+        Select: Prisma.SeoulRentSelect;
+        OrderBy: Prisma.SeoulRentOrderByWithRelationInput;
+        WhereUnique: Prisma.SeoulRentWhereUniqueInput;
+        Where: Prisma.SeoulRentWhereInput;
+        Create: {};
+        Update: {};
+        RelationName: never;
+        ListRelations: never;
+        Relations: {};
+    };
 }
